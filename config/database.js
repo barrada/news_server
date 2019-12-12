0bbb9d4e935bd45e7a31d589981f020c01b5000c
barrada <barrada@me.com>
2019-12-12 16:48:54 +0200

    added new db connection - trying to stop mysql crash

--- config/database.js
+++ config/database.js
@@ -10,17 +10,29 @@ var mysql = require('mysql');
 // });
 
 // remote
+var con;
 
-var con = mysql.createConnection({
-  host: "us-cdbr-iron-east-05.cleardb.net",
-  user: "b11547e48556c8",
-  password: "c33ae484",
-  database:"heroku_4a5d2a3c2d59274"
-});
+function handleDisconnect() {
+  con = mysql.createcon(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
-module.exports=con
+
+
+module.exports=handleDisconnect();
