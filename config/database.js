e0ead1e8eed8f84599dc32d9d51424f7f1a5afe8
barrada <barrada@me.com>
2019-12-12 14:15:12 +0200

    changed db info to remote

--- config/database.js
+++ config/database.js
@@ -1,10 +1,21 @@
 var mysql = require('mysql');
 
+// local 
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database:"puppeteer"
+// });
+
+// remote
+
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database:"puppeteer"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b11547e48556c8",
+  password: "c33ae484",
+  database:"heroku_4a5d2a3c2d59274"
 });
 
 con.connect(function(err) {
