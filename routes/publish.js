3a13bc15c1b9d7d0085aeca53e5b6a48be03db05
barrada <barrada@me.com>
2020-01-29 16:44:01 +0200

    started working on publish

--- routes/publish.js
+++ routes/publish.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+const publishController=require('../controllers/publishController');
 
 /* publish*/
 
-router.post('/', function(req, res, next) {
-  res.status(200).json(
-    {
-      'title':req.body.title,
-      'slug':req.body.slug,
-      'type':req.body.type,
-      'thumbnail':req.body.thumbnail,
-      'tags':req.body.tags,
-      'keywords':req.body.keywords,
-      'excert':req.body.excert,
-      'html':req.body.html,
-      'featured':req.body.featured
+router.post('/', publishController.publish)
+
+// router.post('/', function(req, res, next) {
+//   res.status(200).json(
+//     {
+//       'title':req.body.title,
+//       'slug':req.body.slug,
+//       'type':req.body.type,
+//       'thumbnail':req.body.thumbnail,
+//       'tags':req.body.tags,
+//       'keywords':req.body.keywords,
+//       'excert':req.body.excert,
+//       'html':req.body.html,
+//       'featured':req.body.featured
       
-    }
-    );
-});
+//     }
+//     );
+// });
 
 module.exports = router;
