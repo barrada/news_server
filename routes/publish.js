7e7e2e599de5c79c7c268b5eb03f626bb3d18f2e
barrada <barrada@me.com>
2020-01-29 16:02:25 +0200

    started working on publish

--- routes/publish.js
+++ routes/publish.js
@@ -8,7 +8,13 @@ router.post('/', function(req, res, next) {
   res.status(200).json(
     {
       'title':req.body.title,
-      'slug':req.body.slug
+      'slug':req.body.slug,
+      'slug':req.body.type,
+      'slug':req.body.thumbnail,
+      'slug':req.body.tags,
+      'slug':req.body.keywords,
+      'slug':req.body.excert,
+      'slug':req.body.html
     }
     );
 });
