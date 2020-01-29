116e28043544d55b069035216c37336dfe44e861
barrada <barrada@me.com>
2020-01-29 16:44:36 +0200

    added publish controller and model

---
+++ controllers/publishController.js
@@ -0,0 +1,22 @@
+const publishController={};
+const Publish=require('../models/PublishModel');
+
+// get Single post with slug
+publishController.publish=async function(req, res, next){
+
+	title = req.body.title,
+	slug = req.body.slug,
+	type = req.body.type,
+	thumbnail = req.body.thumbnail,
+	tags = req.body.tags,
+	keywords = req.body.keywords,
+	excert = req.body.excert,
+	html = req.body.html,
+	featured = req.body.featured
+
+	const publish= await Publish.publish(title,slug,type,thumbnail,tags,keywords,excert,html,featured);
+	res.status(201);   
+	
+}
+
+module.exports = postController;
---
+++ models/PublishModel.js
@@ -0,0 +1,15 @@
+const db = require('../config/database');
+
+const Publish={};
+table='posts_copy1'
+
+// publish single post
+
+Publish.publish=async(title,slug,type,thumbnail,tags,keywords,excert,html,featured)=>{
+	single_query = `SELECT * FROM ${table} WHERE slug = '${slug}'`
+
+
+	return "publish model"
+}
+
+module.exports=Publish;
