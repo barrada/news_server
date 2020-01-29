89d4e4f247b8774a67933a721c5d59dc4371f127
barrada <barrada@me.com>
2020-01-29 17:27:06 +0200

    added publish controller and model

--- controllers/publishController.js
+++ controllers/publishController.js
@@ -4,17 +4,17 @@ const Publish=require('../models/PublishModel');
 // get Single post with slug
 publishController.publish=async function(req, res, next){
 
-	title = req.body.title,
-	slug = req.body.slug,
-	type = req.body.type,
-	thumbnail = req.body.thumbnail,
-	tags = req.body.tags,
-	keywords = req.body.keywords,
-	excert = req.body.excert,
-	html = req.body.html,
-	featured = req.body.featured
+	this.title = req.body.title,
+	this.slug = req.body.slug,
+	this.type = req.body.type,
+	this.thumbnail = req.body.thumbnail,
+	this.tags = req.body.tags,
+	this.keywords = req.body.keywords,
+	this.excert = req.body.excert,
+	this.html = req.body.html,
+	this.featured = req.body.featured
 
-	const publish= await Publish.publish(title,slug,type,thumbnail,tags,keywords,excert,html,featured);
+	const publish= await Publish.publish(this.title,this.slug,this.type,this.thumbnail,this.tags,this.keywords,this.excert,this.html,this.featured);
 	res.status(201).json(publish);   
 	
 }
